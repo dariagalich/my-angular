aaf0f9049b25ffe4d11b7a5c7efe0c4ec7fc6696
daria <dariagalich22@gmail.com>
2023-07-17 14:51:46 +0300

    added bootstrap, added header

--- my-angular/src/app/app.module.ts
+++ my-angular/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BaseTypescriptComponent } from './base-typescript/base-typescript.component';
+import {HeaderComponent} from "./components/header/header.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BaseTypescriptComponent
+    BaseTypescriptComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
